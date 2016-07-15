924d0a21a6cdc389778174ee8a97546c71321fba
Nirmalraj J <nirmalraj777@gmail.com>
2016-07-15 12:18:41 +0530

    modified the constants

--- src/main/java/com/gsmart/util/Constants.java
+++ src/main/java/com/gsmart/util/Constants.java
@@ -4,13 +4,5 @@ public class Constants {
 
 	public static final String BAND = "/band";
 	
-	public static final String VIEW_BAND = "/viewBand";
-	
-	public static final String ADD_BAND = "/addBand";
-	
-	public static final String EDIT_BAND = "/editBand";
-	
-	public static final String DELETE_BAND = "/deleteBand";
-	
 	public static final String CONSTRAINT_VIOLATION = "RECORD ALREADY EXISTS";
 }
