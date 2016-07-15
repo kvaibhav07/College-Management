4a02025354ce0c19cc5641a215d7ba61bb403f77
ravi ranjan <rrravirjn11@gmail.com>
2016-07-15 17:34:55 +0530

    added Inventory module

--- src/main/java/com/gsmart/util/Constants.java
+++ src/main/java/com/gsmart/util/Constants.java
@@ -3,6 +3,8 @@ package com.gsmart.util;
 public class Constants {
 
 	public static final String BAND = "/band";
+	public static final String INVENTORY = "/inventory";
+	
 	
 	public static final String CONSTRAINT_VIOLATION = "RECORD ALREADY EXISTS";
 }
