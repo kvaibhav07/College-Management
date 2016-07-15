09ca6b08906ac202e5a59e4b3134a018ed3e1709
bharath <bharathmadesh651@gmail.com>
2016-07-15 18:06:12 +0530

    added set goal

--- src/main/java/com/gsmart/util/Constants.java
+++ src/main/java/com/gsmart/util/Constants.java
@@ -4,5 +4,7 @@ public class Constants {
 
 	public static final String BAND = "/band";
 	
+	public static final String SETGOAL = "/setgoal";
+	
 	public static final String CONSTRAINT_VIOLATION = "RECORD ALREADY EXISTS";
 }
